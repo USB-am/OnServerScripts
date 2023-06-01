c148510301b9b6b3dfedc91264e5640caf4828bc
Цупко Роман Игоревич <smit.acer@mail.ru>
2023-06-01 16:34:50 +0300

    Made a collection of data about the stations and recorded in the db

--- tg_bot/data_base/__init__.py
+++ tg_bot/data_base/__init__.py
@@ -13,13 +13,31 @@ class TelegramUser(db.Model):
 	''' Информация о пользователе бота '''
 
 	__tablename__ = 'telegram_user'
-	# id = db.Column(db.Integer, primary_key=True)
-	chat_id = db.Column(db.Integer, primary_key=True)
+	id = db.Column(db.Integer, primary_key=True)
+	chat_id = db.Column(db.Integer, unique=True)
 	name = db.Column(db.String(255), nullable=False)
 	city = db.Column(db.String(255), default='Москва', nullable=True)
 
 	def __str__(self):
 		return f'<TelegramUser \'{self.name}\'>'
+
+
+class Station(db.Model):
+	''' Информация о станции электрички '''
+
+	__tablename__ = 'train_station'
+	id = db.Column(db.Integer, primary_key=True)
+	yandex_code = db.Column(db.String(63), unique=True)
+	esr_code = db.Column(db.String(63), nullable=True)
+	title = db.Column(db.String(255), nullable=False)
+	type = db.Column(db.String(255), nullable=True)
+	transport = db.Column(db.String(63), nullable=False)
+	settlement = db.Column(db.String(255), nullable=True)
+	region = db.Column(db.String(255), nullable=True)
+	country = db.Column(db.String(255), nullable=False)
+
+	def __str__(self):
+		return f'<{self.type.title()}Station "{self.title}" ({self.transport})>'
 
 
 db.create_all()
---
+++ update_data.py
@@ -0,0 +1,4 @@
+from tg_bot.train_schedules.insert_stations_to_db import start as update_stations
+
+
+update_stations()
