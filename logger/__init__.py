d9846d1d541e5c6ce02ae6b77180584f18541a56
USB-am <smit.acer@mail.ru>
2023-06-27 21:51:06 +0300

    Fix filename bug

--- logger/__init__.py
+++ logger/__init__.py
@@ -5,11 +5,15 @@ from config import LOGS_DIR
 
 
 def save_log(error: Exception) -> None:
-	now_date = datetime.now().date().strftime('YYYYmmdd')
+	now_date = datetime.now().date().strftime('%Y%m%d')
 
 	path_to_now_logs = os.path.join(LOGS_DIR, f'{now_date}.log')
 	with open(path_to_now_logs, mode='a') as log_file:
-		log_file.write(f'[ERROR] {error.__class__.__name__}: {error.args}')
+		error_text = f'\n[ERROR] {error.__class__.__name__}:'
+		for arg in error.args:
+			error_text += f'\n\t{arg}'
+
+		log_file.write(error_text)
 
 
 def logging(func):
