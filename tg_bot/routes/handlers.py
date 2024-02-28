1abb7c7431119b502ab6e752b39cdd18515b715a
Цупко Роман Игоревич <smit.acer@mail.ru>
2024-02-28 17:47:23 +0300

    Finished get_routes function

--- tg_bot/routes/handlers.py
+++ tg_bot/routes/handlers.py
@@ -1,10 +1,10 @@
 import logging
-from datetime import datetime
+from datetime import datetime, date
 
 from telebot import types
 
 from tg_bot.bot import Bot
-from tg_bot.data_base import Station
+from tg_bot.data_base import TelegramUser, Station
 from tg_bot.data_base import handlers as DBHandlers
 from tg_bot.data_base.manager import find_else_create_user
 from .api import get_schedule
@@ -42,6 +42,15 @@ STATION_TYPES = {
 }
 
 
+def is_valid_user_stations(user: TelegramUser) -> bool:
+	''' Проверка соответствия станций у Пользователя '''
+
+	fs = Station.query.get(user.from_station)
+	ts = Station.query.get(user.to_station)
+
+	return not (None in (fs, ts))
+
+
 def user_station_select(message: types.Message, reply_text: str) -> types.Message:
 	''' Выбор пользователем станции '''
 
@@ -51,8 +60,10 @@ def user_station_select(message: types.Message, reply_text: str) -> types.Messag
 
 	stations = Station.query.filter(Station.title.like(f'%{message.text}%'))
 	for station in stations:
-		btn_text = '{title} {transport_type}{station_type}'.format(
+		btn_text = '{transport_type}{station_type} {title} ({address})'.format(
 			title=station.title,
+			address=f'{station.region}, {station.settlement}' \
+				if station.settlement else f'{station.region}',
 			transport_type=TRANSPORT_TYPES[station.transport],
 			station_type=STATION_TYPES[station.type])
 		btn = types.InlineKeyboardButton(btn_text, callback_data=station.yandex_code)
@@ -81,21 +92,67 @@ def change_to_station(message: types.Message)	-> None:
 	__BOT.register_next_step_handler(session, DBHandlers.change_to_station)	
 
 
-def get_routes(message: types.Message) -> str:
-	''' Получить расписание маршрутов '''
+def get_routes(message: types.Message, date_: date=None) -> str:
+	'''
+	Получить расписание маршрутов.
+
+	~params:
+	message: types.Message - объект сообщения Telegram;
+	date_: date=None - дата, на которую необходимо получить расписание.
+	'''
+
+	if date_ is None:
+		date_ = datetime.now().date()
 
 	user = find_else_create_user(message)
 	from_station = Station.query.get(user.from_station)
 	to_station = Station.query.get(user.to_station)
-	schedule_json = get_schedule(from_station, to_station, datetime.now().date())
 
-	output = f'Расписание между {from_station.title}/{to_station.title}\n\n'
+	if not is_valid_user_stations(user):
+		fs = from_station.title if user.from_station else 'Неизвестно'
+		ts = to_station.title if user.to_station else 'Неизвестно'
+		return 'Сначало надо выбрать станции!\n' + \
+			f'- Станция отправления: {fs}\n' + \
+			f'- Станция прибытия: {ts}'
 
-	for segment in schedule_json['segments']:
-		transport_types = ''.join([TRANSPORT_TYPES[transport_type] for transport_type in set(segment['transport_types'])])
-		departure = datetime.fromisoformat(segment['departure'][:-1])
-		arrival = datetime.fromisoformat(segment['arrival'][:-1])
+	schedule_json = get_schedule(from_station, to_station, date_)
 
-		output += f'{departure} ➡ {arrival} {transport_types}\n'
+	output = '{dt}\nРасписание между {from_}/{to}\n\n'.format(
+		from_=from_station.title,
+		to=to_station.title,
+		dt=date_.strftime('%d.%m.%Y')
+	)
+	on_hour = datetime(1970, 1, 1)
+
+	for i, segment in enumerate(schedule_json['segments']):
+		try:
+			transports = (segment['from']['transport_type'],
+				          segment['to']['transport_type'])
+		except KeyError:
+			continue
+		transport_types = ''.join(map(
+			lambda tt: TRANSPORT_TYPES[tt], set(transports)
+		))
+
+		departure = datetime.fromisoformat(segment['departure'][:-6])
+		arrival = datetime.fromisoformat(segment['arrival'][:-6])
+		duration = datetime.fromtimestamp(segment['duration'])
+
+		if on_hour.hour != departure.hour:
+			output += f'\n{departure.strftime("%H")} часов:\n'
+			on_hour = departure
+
+		departure_str = departure.strftime('%H:%M')
+		arrival_str = arrival.strftime('%H:%M')
+		duration = arrival - departure
+
+		duration_hours = int(duration.total_seconds() // 60 // 60)
+		duration_minutes = int(duration.total_seconds() // 60)
+		duration_str = '{days}{hours}{minutes}'.format(
+			days=f'{duration.days}д ' if duration.days else '',
+			hours=f'{duration_hours}ч ' if duration_hours else '',
+			minutes=f'{duration_minutes}м'
+		)
+		output += f'{departure_str} {transport_types} {arrival_str} ⏳{duration_str}\n'
 
 	return output.strip()
